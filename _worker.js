5194c9439e4e6a0dbc869615425a6386f44c9cf4
xxdawa <105995579+xxdawa@users.noreply.github.com>
2023-05-21 10:03:11 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,10 @@
-addEventListener(
-  "fetch",event => {
-     let url=new URL(event.request.url);
-     url.hostname="cdn.jsdelivr.net";  // 你需要反代的域名
-     let request=new Request(url,event.request);
-     event. respondWith(
-       fetch(request)
-     )
-  }
-);
+export default {
+  async fetch(request, env) {
+    const _url = new URL(request.url);
+    _url.hostname = _url.pathname.startsWith("/gh/")
+      ? "cdn.jsdelivr.net"
+      : "github.com";
+    const req = new Request(_url, request);
+    return fetch(req);
+  },
+};

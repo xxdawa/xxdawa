fddb7c8b1901b5c88286c68faa02f7410e4551aa
xxdawa <105995579+xxdawa@users.noreply.github.com>
2023-05-21 09:41:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'www.google.com'
+      url.hostname = 'zh.wikipedia.org'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }

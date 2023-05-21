6d720d5436c24433d441b3924c82f8fee56a8d07
xxdawa <105995579+xxdawa@users.noreply.github.com>
2023-05-21 10:25:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     const _url = new URL(request.url);
     _url.hostname = _url.pathname.startsWith("/gh/")
-      ? "cdn.jsdelivr.net"
+      ? "5best1s.com"
       : "github.com";
     const req = new Request(_url, request);
     return fetch(req);

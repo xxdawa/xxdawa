a18275d0b4665eb1d7b2adaa6ae8f9ab91bed6f4
xxdawa <105995579+xxdawa@users.noreply.github.com>
2023-06-03 05:19:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ export default {
     const _url = new URL(request.url);
     _url.hostname = _url.pathname.startsWith("/5b/")
       ? "5best1s.com"
-      : "github.com";
+      : "gitlab.com";
     const req = new Request(_url, request);
     return fetch(req);
   },

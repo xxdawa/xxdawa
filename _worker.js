396ab337c987705a0cd24bb19fa7cad8654a0fa8
xxdawa <105995579+xxdawa@users.noreply.github.com>
2023-06-03 06:27:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const _url = new URL(request.url);
-    _url.hostname = _url.pathname.startsWith("/vpn-knowledge/")
+    _url.hostname = _url.pathname.startsWith("/gh/")
       ? "5best1s.com"
       : "gitlab.com";
     const req = new Request(_url, request);

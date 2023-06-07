deaf78f530d1bd60e26e8c6d6b6781bee72bb412
xxdawa <105995579+xxdawa@users.noreply.github.com>
2023-06-08 03:54:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,12 @@
 export default {
   async fetch(request, env) {
     const _url = new URL(request.url);
-    _url.hostname = _url.pathname.startsWith("/gh/")
-      ? "5best1s.com"
-      : "gitlab.com";
+    if (_url.pathname.startsWith("/5b/")) {
+      _url.hostname = "5best1s.com";
+      _url.pathname = _url.pathname.slice(3);
+    } else {
+      _url.hostname = "gitlab.com";
+    }
     const req = new Request(_url, request);
     return fetch(req);
   },

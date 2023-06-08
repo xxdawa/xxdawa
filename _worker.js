a560afd1faa2f860366b73d5df59e0c7a5432c1a
xxdawa <105995579+xxdawa@users.noreply.github.com>
2023-06-08 08:59:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const _url = new URL(request.url);
-    if (_url.pathname.startsWith("/5b/")) {
+    if (_url.pathname.startsWith("/5b")) {
       _url.hostname = "5best1s.com";
       _url.pathname = _url.pathname.slice(3);
     } else {
